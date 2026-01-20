9cdd069406b9728d18874f41c6b7cec84f5563ef
Thaneluna <42688917+Thaneluna@users.noreply.github.com>
2026-01-20 12:47:48 -1000

    Updated variable

--- configs/index.js
+++ configs/index.js
@@ -1,7 +1,7 @@
 import { neon } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
 import * as schema from './schema.js';
-const sql = neon(process.env.DATABASE_URL, {
+const sql = neon(import.meta.env.DATABASE_URL, {
     disableWarningInBrowsers: true
 });
 export const db = drizzle({client: sql}, { schema });
